fb31998a32e96ce889c777da1d7bd1e74b70d5fe
Kaan <kaan.kalmioglu@hva.nl>
2024-03-12 09:14:40 +0100

    Data fetching test
    Test 1 works (SDG)

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Importeer het npm pakket express uit de node_modules map
 import express from 'express'
 
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from './helpers/fetch-json.js'
+
 // Maak een nieuwe express app aan
 const app = express()
 
@@ -18,14 +21,18 @@ app.use(express.urlencoded({ extended: true }))
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8000)
 
+
+const apiUrl = 'https://fdnd-agency.directus.app/items'
+// Maak een GET route voor de index
+app.get('/', async function (request, response) {
+  // Render index.ejs uit de views map
+  const sdgData = await fetchJson(apiUrl + '/hf_sdgs')
+  response.render('index', { sdgs: sdgData.data });
+});
+
+
 // Start express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
-
-// Maak een GET route voor de index
-app.get('/', function (request, response) {
-      // Render index.ejs uit de views map
-      response.render('index');
-    });
