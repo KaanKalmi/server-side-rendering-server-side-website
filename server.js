022655c6f37790c4204a027e61caf239fc5ad007
Kaan <144000125+KaanKalmi@users.noreply.github.com>
2024-03-14 09:30:17 +0100

    Begin dashboard

--- server.js
+++ server.js
@@ -16,23 +16,34 @@ app.set('views', './views')
 // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static('public'))
 
-app.use(express.urlencoded({ extended: true }))
+// Zorg dat werken met request data makkelijker wordt
+app.use(express.urlencoded({extended: true}))
+
+
+// Stel het basis endpoint in
+const apiUrl = 'https://fdnd-agency.directus.app/items'
+
+const sdgData = await fetchJson(apiUrl + '/hf_sdgs')
+const stakeholdersData = await fetchJson(apiUrl + '/hf_stakeholders')
+const scoresData = await fetchJson(apiUrl + '/hf_scores')
+const companiesData = await fetchJson(apiUrl + '/hf_companies/1')
+
+console.log(companiesData.data.name)
+
+app.get('/', function(request, response) {
+        response.render('index', {
+            sdg: sdgData.data,
+            stakeholder: stakeholdersData.data,
+            score: scoresData.data,
+            companie: companiesData.data
+        })
+})
 
 // Stel het poortnummer in waar express op moet gaan luisteren
-app.set('port', process.env.PORT || 8000)
-
-
-const apiUrl = 'https://fdnd-agency.directus.app/items'
-// Maak een GET route voor de index
-app.get('/', async function (request, response) {
-  // Render index.ejs uit de views map
-  const sdgData = await fetchJson(apiUrl + '/hf_sdgs')
-  response.render('index', { sdgs: sdgData.data });
-});
-
+app.set('port', process.env.PORT || 8009)
 
 // Start express op, haal daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get('port'), function () {
+app.listen(app.get('port'), function() {
   // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
+    console.log(`Application started on http://localhost:${app.get('port')}`)
 })
